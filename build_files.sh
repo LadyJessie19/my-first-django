09dff98905fd953516d9d7b837654c4fe909bbae
Jessie M. Bentes <jessie.moura19@gmail.com>
2025-03-04 11:04:57 -0400

    fix: fourth attempt to deploy. y-a-y 😒

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
