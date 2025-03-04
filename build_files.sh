1f3efcdea891de79cb43d0bb48c55a1b3f0812a8
Jessie M. Bentes <jessie.moura19@gmail.com>
2025-03-04 10:55:19 -0400

    fix: third attempt to deploy

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput --clear
+python -m pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
